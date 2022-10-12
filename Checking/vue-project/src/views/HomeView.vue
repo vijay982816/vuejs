863767ee1f253eb270110a3bd9b6a7563c1d7b2a
Aman982816 <aman982816aman@gmail.com>
2022-10-12 14:44:39 +0000

    done

--- Checking/vue-project/src/views/HomeView.vue
+++ Checking/vue-project/src/views/HomeView.vue
@@ -1,14 +1,136 @@
-<script setup>
+<script >
+
+import Navbar from '../components/Navbar.vue'
+
+  components:{
+        Navbar
+     
+    }
+    
 
 </script>
 
 <template>
-  <main>
-Home page 
 
-  <h1 className="text-3xl font-bold underline bg-slate-400">
-      Hello world!
-    </h1>
 
-  </main>
+<div>
+  <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+    <span class="font-semibold text-xl tracking-tight">Vue js app</span>
+  </div>
+  <div class="block lg:hidden">
+    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    </button>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="text-sm lg:flex-grow">
+      
+    </div>
+    <div>
+      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Register</a>
+
+            <a href="#" class="ml-10 inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Signup</a>
+    </div>
+
+  </div>
+</nav>
+  </div>
+
+
+
+    <div className="antialiased font-sans bg-gray-200">
+                <div className="container mx-auto px-4 sm:px-8">
+                    <div className="py-8 lg:px-60">
+                        <div>
+                            <h2 className="text-2xl font-semibold leading-tight">Users</h2>
+                        </div>
+
+                        <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                                <table className="min-w-full leading-normal">
+                                    <thead>
+                                        <tr>
+                                            <th
+                                                className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                                User
+                                            </th>
+                                            <th
+                                                className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                                email
+                                            </th>
+                                            <th
+                                                className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                                phone
+                                            </th>
+                                            <th
+                                                className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                                city
+                                            </th>
+                                         
+                                            <th
+                                                className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                                Action
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <!-- <tbody>
+
+
+                                        {users.map((element) => {
+
+
+
+
+                                            const base64String = btoa(
+                                                String.fromCharCode(...new Uint8Array(element.profileImage.data.data))
+                                            );
+
+
+
+                                            return <User key={element._id} id={element._id} imgUrl={`data:${element.profileImage.contentType};base64,${base64String}`} name={element.name} age={element.age} />
+                                        }
+
+
+
+
+
+
+                                        )}
+
+                                    </tbody> -->
+                                </table>
+
+
+                                <div
+                                    className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
+
+                                    <div className="inline-flex mt-2 xs:mt-0">
+                                        <button
+                                            className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
+
+
+
+                                            <!-- <Link to="/adduser">Add User</Link> -->
+
+
+Add user
+
+                                        </button>
+
+
+
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
 </template>
---
+++ Checking/vue-project/src/views/Register.vue
@@ -0,0 +1,113 @@
+<template>
+<div >
+
+                <div className='py-12 bg-gray-700 transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0  ' id="modal">
+
+
+
+
+
+   <form @submit="onSubmit" class="add-form container mx-auto w-11/12 md:w-2/3 max-w-lg">
+  
+
+    <div class="form-control">
+           <label className="text-white text-sm font-bold leading-tight tracking-normal">Name</label>
+                            <input id="name" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Ex. Hitesh"  v-model="name" name='name' />
+
+
+
+    </div>
+  
+    <div class="form-control">
+           <label className="text-white text-sm font-bold leading-tight tracking-normal">Email</label>
+                            <input id="email" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Ex. name@gmail.com"  v-model="email" name='email' />
+
+
+
+    </div>
+
+      <div class="form-control">
+           <label className="text-white text-sm font-bold leading-tight tracking-normal">City</label>
+                            <input id="city" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Ex. Jaipur"  v-model="city" name='city' />
+
+
+
+    </div>
+
+      <div class="form-control">
+           <label className="text-white text-sm font-bold leading-tight tracking-normal">Password</label>
+                            <input id="password" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Ex. Password@123"  v-model="Password" name='password' />
+
+
+
+    </div>
+
+<input type="submit" value="Save Information" class="btn btn-block bg-white px-3 oy-3" />
+  </form>
+
+                </div>
+                </div>
+
+
+
+
+</template>
+
+<script>
+export default {
+name:"Register",
+ data (){
+            return {
+                name : '',
+                email : '',
+                phone : null,
+                city : '',
+
+            }
+        },
+
+methods:{
+   onSubmit(e){
+
+
+    // console.log(this.email,this.phone,this.city,this.password)
+    // alert(this.city)
+
+//    alert(this.email)
+
+                // e.preventDefault()
+                if(!this.name||!this.phone || !this.city || !this.email){
+                    alert('Please fill all the detials')
+                    return
+                }
+                const NewInformation = {
+                  name : this.name,
+                    email : this.email,
+                    phone : this.phone,
+                    city : this.city,
+
+                }
+                
+                this.$emit('add-information', newInformation)
+                this.name = ' ',
+                this.age = ' ',
+                this.phone = null,
+                this.city = ''
+
+
+
+
+            }
+
+
+     
+}
+
+
+
+}
+</script>
+
+<style>
+
+</style>
