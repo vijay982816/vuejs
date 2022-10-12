3715a5dffb8c1b04d38c2557d5932ad4918fc7d8
Aman982816 <aman982816aman@gmail.com>
2022-10-12 13:17:59 +0000

    new project creted with tailwind css configuration

---
+++ Checking/vue-project/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+Home page 
+
+  <h1 className="text-3xl font-bold underline bg-slate-400">
+      Hello world!
+    </h1>
+
+  </main>
+</template>
---
+++ src/Views/Home.vue
@@ -0,0 +1,29 @@
+
+<script >
+    import AllUserVue from '../components/AllUser.vue'
+    import UsingBootstrap from '../components/UsingBootsrap.vue'
+
+export default {
+    name : "Home",
+    components:{
+        AllUserVue,
+        UsingBootstrap
+    }
+    
+}
+
+
+</script>
+<template>
+
+
+<AllUserVue />
+
+<!-- <UsingBootstrap/> -->
+</template>
+
+
+
+<style scoped>
+
+</style>
