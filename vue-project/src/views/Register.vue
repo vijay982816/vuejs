9979029d0d8ad18bfb931f60565d440d81045d62
vijay982816 <94524975+vijay982816@users.noreply.github.com>
2022-10-13 09:54:14 +0000

    register done

--- vue-project/src/views/Register.vue
+++ vue-project/src/views/Register.vue
@@ -7,7 +7,7 @@
 
 
 
-   <form @submit="onSubmit" class="add-form container mx-auto w-11/12 md:w-2/3 max-w-lg">
+   <form  class="add-form container mx-auto w-11/12 md:w-2/3 max-w-lg">
   
 
     <div class="form-control">
@@ -17,17 +17,17 @@
 
 
     </div>
-    <div class="form-control">
-           <label className="text-white text-sm font-bold leading-tight tracking-normal">Name</label>
-                            <input id="name" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Ex. Hitesh"  v-model="name" name='name' />
-
-
-
-    </div>
-  
     <div class="form-control">
            <label className="text-white text-sm font-bold leading-tight tracking-normal">Email</label>
-                            <input id="email" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Ex. name@gmail.com"  v-model="email" name='email' />
+                            <input id="name" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Ex. name@gmail.com"  v-model="email" name='email' />
+
+
+
+    </div>
+  
+    <div class="form-control">
+           <label className="text-white text-sm font-bold leading-tight tracking-normal">phone</label>
+                            <input id="email" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Ex. 9898989898 "  v-model="phone" name='phone' />
 
 
 
@@ -41,22 +41,86 @@
 
     </div>
 
-      <div class="form-control">
-           <label className="text-white text-sm font-bold leading-tight tracking-normal">Password</label>
-                            <input id="password" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Ex. Password@123"  v-model="Password" name='password' />
 
 
 
-    </div>
-
-<input type="submit" value="Save Information" class="btn btn-block bg-white px-3 oy-3" />
+<button class="btn btn-block bg-white px-5 py-3 rounded-lg " @click="adduser" >add user</button>
   </form>
 
                 </div>
+         
+
+
+
+
+
+                    <div >
+
+                <div className='py-12 bg-gray-700 transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0 ' id="modal">
+
+
+
+
+              
+
+
+
+
+
+                    <div role="alert" className="container mx-auto w-11/12 md:w-2/3 max-w-lg">
+                        <div className="relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
+                       
+                            <h1 className="text-gray-800 font-lg font-bold tracking-normal leading-tight mb-4">Enter User Details</h1>
+                            <label htmlFor="name" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">User Name</label>
+                            <input id="name" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Ex. Hitesh"  v-model="name" name='name' />
+
+
+
+
+
+
+                            <label htmlFor="email" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">Email</label>
+                            <input id="email" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"  placeholder="Ex. Hitesh@gmail.com"  v-model="email" name='email' />
+
+                            <label htmlFor="email" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">city</label>
+                            <input id="email" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"  placeholder="Ex.jaipur"  v-model="city" name='city' />
+
+                            <label htmlFor="email" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">phone</label>
+                            <input id="email" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"  placeholder="Ex. 9898989898"  v-model="phone" name='phone' />
+
+
+
+
+                            
+
+
+
+
+
+
+                            <div className="flex items-center justify-start w-full">
+                                <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm"
+                                @click="adduser"
+                                
+                                >Submit</button>
+
+
+
+                                <RouterLink to="/" className="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm" >Cancel</RouterLink>
+
+                            </div>
+                           
+                        </div>
+                    </div>
                 </div>
 
+            </div>
 
 
+
+
+
+                   </div>
 
 </template>
 
@@ -67,13 +131,32 @@ name:"Register",
             return {
                 name : '',
                 email : '',
-                phone : null,
+                phone : '',
                 city : '',
 
             }
         },
 
 methods:{
+
+async adduser (){
+
+console.log(this.name,this.email,this.phone,this.city)
+
+const addedUser = await fetch('http://example.com/movies.json',{method:'POST',body:{
+  name:this.name,phone:this.phone,email:this.email,city:this.city
+}})
+  .then((async (response) => await response.json()))
+  .catch((error) => console.log(error.message));
+
+
+  console.log(addedUser)
+
+
+
+
+
+}
 
 
 
