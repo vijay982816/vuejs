383b965b739b4e2fa4d7078c12cf534ac638670a
vijay982816 <94524975+vijay982816@users.noreply.github.com>
2022-10-13 09:20:22 +0000

    router link to register page

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -119,10 +119,9 @@
                                             className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
 
 
+<RouterLink to="/register">Add user</RouterLink>
 
-
-
-Add user
+ 
 
 
 
